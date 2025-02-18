36ce3e58e8a285fe5fcd02adf8949532891b8d90
Adarsh Diwakar <103514781+sirdiwakar@users.noreply.github.com>
2025-02-18 08:53:31 +0530

    Update Navbar.jsx

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <img className='logo' src={assets.logo} alt="" />
+        <p>Welcome Adarsh Diwakar</p>
         <img src={assets.profile_image} alt="" className="profile" />
       
     </div>
